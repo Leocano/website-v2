7212103f4720e9b65600fb98d2a1d01bc2e23776
Leonardo Cano <leocano4242@gmail.com>
2025-08-08 00:37:12 -0300

    Add my domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://leocano.dev",
   vite: {
     plugins: [tailwindcss()],
   },
