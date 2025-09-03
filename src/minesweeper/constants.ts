b07358119ff5da0f18363480590938669fb7f6d7
Leonardo Cano <leocano4242@gmail.com>
2025-09-03 18:38:02 -0300

    Fix directions

--- src/minesweeper/constants.ts
+++ src/minesweeper/constants.ts
@@ -7,7 +7,11 @@ export const MINE_AMOUNT = 40;
 
 export const DIRECTIONS = [
   [0, 1], // right
+  [-1, 1], // up-right
+  [1, 1], // down-right
   [0, -1], // left
+  [-1, -1], // up-left
+  [1, -1], // down-left
   [1, 0], // down
   [-1, 0], // up
 ];
