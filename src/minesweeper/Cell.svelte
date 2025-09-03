aee5f5ac8b21e3f900a7f8f89df0b6980a63e5f8
Leonardo Cano <leocano4242@gmail.com>
2025-09-03 00:48:49 -0300

    Remove useless directions

--- src/minesweeper/Cell.svelte
+++ src/minesweeper/Cell.svelte
@@ -11,11 +11,7 @@
     [0, 1], // right
     [0, -1], // left
     [1, 0], // down
-    [1, 1], // down-right
-    [1, -1], // down-left
     [-1, 0], // up
-    [-1, 1], // up-right
-    [-1, -1], // up-left
   ];
 
   function floodFill(row: number, col: number) {
--- src/minesweeper/board/boardState.svelte.ts
+++ src/minesweeper/board/boardState.svelte.ts
@@ -17,11 +17,7 @@ const setup = (): Cell[][] => {
     [0, 1], // right
     [0, -1], // left
     [1, 0], // down
-    [1, 1], // down-right
-    [1, -1], // down-left
     [-1, 0], // up
-    [-1, 1], // up-right
-    [-1, -1], // up-left
   ];
 
   function fillBombCount(row: number, col: number) {
