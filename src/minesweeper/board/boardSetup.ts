5a89cb4f2f8d6a9728ec4add1e48e4761b2b61fb
Leonardo Cano <leocano4242@gmail.com>
2025-09-04 00:15:07 -0300

    Refactor board setup

--- src/minesweeper/board/boardSetup.ts
+++ src/minesweeper/board/boardSetup.ts
@@ -2,42 +2,44 @@ import { BOARD_SIZE, MINE_AMOUNT, DIRECTIONS } from "../constants";
 import type { Cell, Board } from "../types";
 import { isOutOfBounds } from "./utils";
 
-const initialBoard = Array.from(Array(BOARD_SIZE.ROWS), () =>
-  Array.from(
-    Array(BOARD_SIZE.COLS),
-    (): Cell => ({
-      content: "empty",
-      status: "initial",
-      bombsAround: 0,
-    }),
-  ),
-);
-
-function incrementBombCount(row: number, col: number) {
-  if (isOutOfBounds(row, col)) {
-    return;
-  }
-
-  initialBoard[row][col].bombsAround++;
+function createEmptyBoard() {
+  return Array.from(Array(BOARD_SIZE.ROWS), () =>
+    Array.from(
+      Array(BOARD_SIZE.COLS),
+      (): Cell => ({
+        content: "empty",
+        status: "initial",
+        bombsAround: 0,
+      }),
+    ),
+  );
 }
 
 export function setup(): Board {
-  for (let i = 0; i < MINE_AMOUNT; i++) {
-    const randomRow = Math.floor(Math.random() * BOARD_SIZE.ROWS);
-    const randomCol = Math.floor(Math.random() * BOARD_SIZE.COLS);
-    const cell = initialBoard[randomRow][randomCol];
+  const board = createEmptyBoard();
+
+  let placed = 0;
+  while (placed < MINE_AMOUNT) {
+    const row = Math.floor(Math.random() * BOARD_SIZE.ROWS);
+    const col = Math.floor(Math.random() * BOARD_SIZE.COLS);
+
+    const cell = board[row][col];
 
     if (cell.content === "mine") {
-      i--;
       continue;
     }
 
     cell.content = "mine";
+    placed++;
 
     for (const [rowOffset, colOffset] of DIRECTIONS) {
-      incrementBombCount(randomRow + rowOffset, randomCol + colOffset);
+      const newRow = row + rowOffset;
+      const newCol = col + colOffset;
+      if (!isOutOfBounds(newRow, newCol)) {
+        board[newRow][newCol].bombsAround++;
+      }
     }
   }
 
-  return initialBoard;
+  return board;
 }
